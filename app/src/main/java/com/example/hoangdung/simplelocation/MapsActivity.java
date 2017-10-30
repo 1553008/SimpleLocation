979cf60418bd9f706e72c649f2d531c36f623a60
lhdung1997 <lhdung1997@gmail.com>
2017-10-30 21:03:38 +0700

    MapsActivity showLastknownLocation added

--- app/src/main/java/com/example/hoangdung/simplelocation/MapsActivity.java
+++ app/src/main/java/com/example/hoangdung/simplelocation/MapsActivity.java
@@ -1,18 +1,29 @@
 package com.example.hoangdung.simplelocation;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
@@ -24,7 +35,16 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     //For Debugging
     private String TAG = MapsActivity.class.getSimpleName();
+    private int DEFAULT_ZOOM = 15;
+    //Google Map model
     private GoogleMap mMap;
+
+    //Fused Location Provider
+    private FusedLocationProviderClient mLocationProvider;
+
+    //Last known location
+    private Location mLastknownLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +53,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+        mLocationProvider = LocationServices.getFusedLocationProviderClient(this);
+    }//onCreate
 
 
     /**
@@ -48,10 +69,38 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        updateUI();
+        showLastknownLocation();
+    }//onMapReady
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private void showLastknownLocation() {
+       try{
+           Task<Location> lastLocation = mLocationProvider.getLastLocation();
+           lastLocation.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+               @Override
+               public void onComplete(@NonNull Task<Location> task) {
+                   if (task.isComplete() && task.isSuccessful()) {
+                       mLastknownLocation = task.getResult();
+                       mMap.moveCamera(CameraUpdateFactory
+                               .newLatLngZoom(
+                                       new LatLng(mLastknownLocation.getLatitude(), mLastknownLocation.getLongitude())
+                                       , DEFAULT_ZOOM));
+                   }
+               }
+           });
+       }
+       catch (SecurityException e){
+
+       }
+
+    }// showLastknowLocation
+
+    private void updateUI() {
+        try{
+           mMap.setMyLocationEnabled(true);
+        }
+        catch (SecurityException e){
+
+        }
     }
 }
